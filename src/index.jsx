c25fe22d76c7fe43efe4f990d7fd8956a9168a80
julienemo <juliette.nada@gmail.com>
2020-05-04 16:16:10 +0200

    [change] looping image calling methode - using inline "require" instead of "import" - note how the relative path from NODE_PATH still works - note nested braces

--- src/index.jsx
+++ src/index.jsx
@@ -4,19 +4,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MixedComponent from "./components/MixedComponents";
 
-import cat_on_balcony from "assets/img/cat_on_balcony.png";
-import asperges from "assets/img/asperges.png";
-import lantern from "assets/img/lantern.png";
-
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <MixedComponent text="cat" title="Cat" src={cat_on_balcony} />
-        <MixedComponent text="food" title="Food" src={asperges} />
-        <MixedComponent text="lantern" title="Lantern" src={lantern} />
-      </div>
-    );
+    const components = [
+      {
+        title: "Cat",
+        text: "View of a cat sitting on the balcony",
+        src: "cat_on_balcony",
+      },
+      {
+        title: "Food",
+        text:
+          "Cooked green asperges on the plate with chopped garlic over them",
+        src: "asperges",
+      },
+      {
+        title: "Craft",
+        text: "Hand made Jack o Lantern on a glowing light bulb",
+        src: "lantern",
+      },
+    ];
+    const content = components.map((el) => (
+      <MixedComponent
+        text={el.text}
+        title={el.title}
+        src={require(`assets/img/${el.src}.png`)}
+      />
+    ));
+    return content;
   }
 }
 
